88ff3db32397acde7c222c6e4c763fcdaa10efd8
Felipe <felipe.araujo@hubfintech.com.br>
2022-02-04 20:22:41 -0300

    feat: :sparkles: files for using singletons

---
+++ src/design-patterns/creational/singleton/module-a.ts
@@ -0,0 +1,8 @@
+import { MyDatabaseFunction } from "./my-database-function";
+import "./module-b";
+
+const myDatabaseClassic = MyDatabaseFunction;
+myDatabaseClassic.add({ name: "TesteA1", age: 26 });
+myDatabaseClassic.add({ name: "TesteA2", age: 25 });
+myDatabaseClassic.add({ name: "TesteA3", age: 50 });
+myDatabaseClassic.show();
---
+++ src/design-patterns/creational/singleton/module-b.ts
@@ -0,0 +1,6 @@
+import { MyDatabaseFunction } from "./my-database-function";
+
+const myDatabaseClassic = MyDatabaseFunction;
+myDatabaseClassic.add({ name: "TesteB1", age: 26 });
+myDatabaseClassic.add({ name: "TesteB2", age: 25 });
+myDatabaseClassic.add({ name: "TesteB3", age: 50 });
