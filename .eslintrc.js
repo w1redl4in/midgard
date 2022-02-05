9375ba08e2cc652e6b56e7ae14c63b1b40c86f76
Felipe <felipe.araujo@hubfintech.com.br>
2022-02-05 00:12:57 -0300

    chore: :rotating_light: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    'import/extensions': 'off',
+    'no-useless-constructor': 'off',
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-underscore-dangle': 'off',
+    'no-restricted-syntax': 'off',
+    'no-console': 'off',
+    'wrap-iife': 'off',
+    'func-names': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
